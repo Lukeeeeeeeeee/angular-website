413548062fb68c199150d801068871b7ef3e0aa4
SamoZ <15176981781@163.com>
2018-11-28 13:52:57 +0800

    ng-reactive-forms

---
+++ src/app/profile-editor/profile-editor.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@angular/forms';
+
+@Component({
+    selector: 'app-profile-editor',
+    templateUrl: './profile-editor.component.html',
+    styleUrls: ['./profile-editor.component.css']
+})
+export class ProfileEditorComponent implements OnInit {
+    // profileForm = new FormGroup({
+    //     firstName: new FormControl(''),
+    //     lastName: new FormControl(''),
+    //     address: new FormGroup({
+    //         street: new FormControl(''),
+    //         city: new FormControl(''),
+    //         state: new FormControl(''),
+    //         zip: new FormControl('')
+    //     })
+    // });
+
+    profileForm = this.fb.group({
+        firstName: ['', Validators.required],
+        lastName: [''],
+        address: this.fb.group({
+            street: [''],
+            city: [''],
+            state: [''],
+            zip: ['']
+        }),
+        aliases: this.fb.array([
+            this.fb.control('')
+        ])
+    });
+
+    get aliases() {
+        return this.profileForm.get('aliases') as FormArray;
+    }
+
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit() {}
+
+    onSubmit() {
+        console.warn(this.profileForm.value);
+    }
+
+    updateProfile() {
+        this.profileForm.patchValue({
+            firstName: 'Nancy',
+            address: {
+                street: '123 Drew Street'
+            }
+        });
+    }
+
+    addAlias() {
+        this.aliases.push(this.fb.control(''));
+    }
+}

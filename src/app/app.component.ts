ef4eb703eeb8b5aec772d63bbf917cafc1f24103
SamoZ <15176981781@163.com>
2018-11-30 17:28:27 +0800

    ng-dynamic-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { QuestionService } from './services/question.service';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [QuestionService]
 })
 export class AppComponent {
-  title = 'app';
+    questions: any[];
+
+    constructor(service: QuestionService) {
+        this.questions = service.getQuestions();
+    }
 }
---
+++ src/app/dynamic-form-question/dynamic-form-question.component.ts
@@ -0,0 +1,21 @@
+import { FormGroup } from '@angular/forms';
+import { QuestionBase } from './../question-base';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-dynamic-form-question',
+    templateUrl: './dynamic-form-question.component.html',
+    styleUrls: ['./dynamic-form-question.component.css']
+})
+export class DynamicFormQuestionComponent implements OnInit {
+    @Input() question: QuestionBase<any>;
+    @Input() form: FormGroup;
+
+    constructor() {}
+
+    ngOnInit() {}
+
+    get isValid() {
+        return this.form.controls[this.question.key].valid;
+    }
+}

872523ccf1f394de1b15f9e6df6698860986b117
SamoZ <15176981781@163.com>
2019-01-08 17:29:19 +0800

    ng-animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OpenCloseComponent } from './open-close/open-close.component';
+import { InsertRemoveComponent } from './insert-remove/insert-remove.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, OpenCloseComponent, InsertRemoveComponent],
+    imports: [BrowserModule, BrowserAnimationsModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/insert-remove/insert-remove.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+@Component({
+    selector: 'app-insert-remove',
+    templateUrl: './insert-remove.component.html',
+    styleUrls: ['./insert-remove.component.css'],
+    animations: [
+        trigger('myInsertRemoveTrigger', [
+            transition(':enter', [
+                style({opacity: 0}),
+                animate('5s', style({opacity: 1})),
+            ]),
+            transition(':leave', [
+                animate('5s', style({opacity: 0})),
+            ])
+        ])
+    ]
+})
+export class InsertRemoveComponent implements OnInit {
+    isShown = false;
+
+    constructor() {}
+
+    ngOnInit() {}
+
+    toggle() {
+        this.isShown = !this.isShown;
+    }
+}
